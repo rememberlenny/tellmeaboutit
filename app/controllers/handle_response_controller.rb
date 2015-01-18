d5dab369720cdf1db2887b4502b4c0bdc694da2d
rememberlenny <rememberlenny@gmail.com>
2015-01-18 12:01:34 -0500

    Save the account

--- app/controllers/handle_response_controller.rb
+++ app/controllers/handle_response_controller.rb
@@ -5,4 +5,9 @@ class HandleResponseController < ApplicationController
     a = Account.new(:recording_url => recording, :recording_length => length)
     a.save
   end
+
+  private
+    def handle_response_params
+      params.require(:account).permit(:recording_url, :recording_length, :account_id, :created_at, :updated_at)
+    end
 end
