75fcdb1600ab53bc4a557eb9f20ee76ae3917f68
rememberlenny <rememberlenny@gmail.com>
2015-01-18 11:46:22 -0500

    Save the null session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'say_intro' =>        'twilio_responses#say_intro'
   match 'get_id' =>          'twilio_responses#id_number', :via => [:post, :get]
-  match 'handle_response' =>  'handle_response#save', :via => [:post, :get]
+  match 'handle_response' =>  'handle_response#save',      :via => [:post, :get]
 
   root 'static_page#home'
 end
