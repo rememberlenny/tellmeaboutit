099c6e44324b0db6e0370f7ba78bd820099f59f5
rememberlenny <rememberlenny@gmail.com>
2015-01-18 12:56:37 -0500

    Save the time

--- app/controllers/handle_response_controller.rb
+++ app/controllers/handle_response_controller.rb
@@ -4,5 +4,15 @@ class HandleResponseController < ApplicationController
     length = params[:RecordingDuration]
     a = Account.new(:recording_url => recording, :recording_length => length)
     a.save
+    response = double_check
+    render text: response
+  end
+
+  def double_check
+    response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    response << "<Response>";
+    response << "<Say>This works!</Say>";
+    response << "</Response>";
+    return response
   end
 end
