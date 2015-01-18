38ea76c7b3b2d0cba7a553acfb964360d905fda5
rememberlenny <rememberlenny@gmail.com>
2015-01-18 13:23:21 -0500

    Check the recording

--- app/controllers/handle_response_controller.rb
+++ app/controllers/handle_response_controller.rb
@@ -8,6 +8,18 @@ class HandleResponseController < ApplicationController
     render text: response
   end
 
+  def check_response
+    response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    response << "<Response>";
+    response << "<Gather timeout=\"10\" action=\"https://tellmeaboutit.herokuapp.com/check_recording\" numDigits=\"1\">"
+    response << "<Say>If you are happy with your recording, press pound.</Say>"
+    response << "<Say>If you would like to listen to the recording, press 1.</Say>"
+    response << "<Say>If you would like to re-record your message, ppress *.</Say>"
+    response << "</Gather>"
+    response << "</Response>";
+    return response
+  end
+
   def double_check
     response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     response << "<Response>";
