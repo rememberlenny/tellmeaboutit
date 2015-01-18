64b0b3c56ed9c209f395bf5570d6e3d73c742d45
rememberlenny <rememberlenny@gmail.com>
2015-01-18 13:17:06 -0500

    Save the method get

--- app/controllers/twilio_responses_controller.rb
+++ app/controllers/twilio_responses_controller.rb
@@ -51,17 +51,17 @@ class TwilioResponsesController < ApplicationController
     response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
     response << "<Response>";
     if account != nil
-      response << "<Record transcribe=\"true\" finishOnKey=\"#\" maxLength=\"45\" transcribeCallback=\"https://tellmeaboutit.herokuapp.com/handle_response\"/>";
+      response << "<Record transcribe=\"true\" finishOnKey=\"#\" maxLength=\"45\" method=\"GET\" action=\"https://tellmeaboutit.herokuapp.com/handle_response\"/>";
       response << "<Gather timeout=\"10\" action=\"https://tellmeaboutit.herokuapp.com/check_recording\" numDigits=\"1\">"
         response << "<Say>If you are happy with your recording, press pound.</Say>"
         response << "<Say>If you would like to listen to the recording, press 1.</Say>"
         response << "<Say>If you would like to re-record your message, ppress *.</Say>"
       response << "</Gather>"
-    else
-      response << "<Play>https://s3-us-west-1.amazonaws.com/tellmeabout/3-please-enter-your-id.wav</Play>";
-      response << "<Record action=\"https://tellmeaboutit.herokuapp.com/handle_response\" method=\"GET\">";
-      response << " <Play>https://s3-us-west-1.amazonaws.com/tellmeabout/3-please-enter-your-id.wav</Play>";
-      response << "</Record>";
+    # else
+    #   response << "<Play>https://s3-us-west-1.amazonaws.com/tellmeabout/3-please-enter-your-id.wav</Play>";
+    #   response << "<Record action=\"https://tellmeaboutit.herokuapp.com/handle_response\" method=\"GET\">";
+    #   response << " <Play>https://s3-us-west-1.amazonaws.com/tellmeabout/3-please-enter-your-id.wav</Play>";
+    #   response << "</Record>";
     end
     response << "</Response>";
     return response
