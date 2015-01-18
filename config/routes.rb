3f2a982d60ea2aaf59424680658d74915c350c4e
rememberlenny <rememberlenny@gmail.com>
2015-01-18 11:41:13 -0500

    Get the post information

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'say_intro' =>        'twilio_responses#say_intro'
-  get 'get_id' =>           'twilio_responses#id_number'
+  match 'get_id' =>          'twilio_responses#id_number', :via => [:post]
   get 'handle_response' =>  'handle_response#save'
 
   root 'static_page#home'
