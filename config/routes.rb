badc27762c229c4b5150374d79dae5c0dfdcc47d
rememberlenny <rememberlenny@gmail.com>
2015-01-18 14:22:51 -0500

    Save

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'say_intro' =>        'twilio_responses#say_intro'
+  get 'get' =>              'static_pages#home'
   match 'get_id',          to: 'twilio_responses#id_number',       :via => [:post, :get]
   match 'handle_response', to: 'handle_response#save',             :via => [:post, :get]
   match 'check_recording', to: 'twilio_responses#check_recording', :via => [:post, :get]
   match 'check_response',  to: 'twilio_responses#check_response',  :via => [:post, :get]
   match 'after_recording', to: 'twilio_responses#after_recording', :via => [:post, :get]
 
-  root 'static_pages#home'
+  root 'static_pages#signup'
 end
