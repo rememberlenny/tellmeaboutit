7414090ddca103525216d6ff3e5b6bb0ed083ad4
lamthuyvo <lamthuyvo@gmail.com>
2015-01-18 13:39:17 -0500

    added content and nav bar to the static_pages home.html.erb file

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	var	screen_width = window.innerWidth;
+	var	screen_height = window.innerHeight;
+
+
+	$(".fullscreen").css("width", screen_width);
+	$(".fullscreen").css("height", screen_height);
+		
+
+
+});
