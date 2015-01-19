7ae53b58c9202c54b27429d705b56ad2e2076a07
rememberlenny <rememberlenny@gmail.com>
2015-01-19 11:55:10 -0500

    Create the relationships

--- db/migrate/20150119163906_create_stories.rb
+++ db/migrate/20150119163906_create_stories.rb
@@ -1,7 +1,10 @@
 class CreateStories < ActiveRecord::Migration
   def change
     create_table :stories do |t|
-
+      t.text :transcript
+      t.string :name
+      t.integer :recording_id
+      t.integer :details_id
       t.timestamps null: false
     end
   end
