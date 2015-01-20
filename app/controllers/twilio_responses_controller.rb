6011c1e284985c6301ab245c3e8ef150b884c801
rememberlenny <rememberlenny@gmail.com>
2015-01-20 00:32:53 -0500

    Change over to twilio_responses

--- app/controllers/twilio_responses_controller.rb
+++ app/controllers/twilio_responses_controller.rb
@@ -3,6 +3,23 @@ class TwilioResponsesController < ApplicationController
     return 'http://tellmebout.it'
   end
 
+  def save
+    recording = params[:RecordingUrl]
+    length = params[:RecordingDuration]
+    a = Account.new(:recording_url => recording, :recording_length => length)
+    a.save
+    response = double_check
+    render text: response
+  end
+
+  def double_check
+    response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+    response << "<Response>";
+    response << "<Say>This works!</Say>";
+    response << "</Response>";
+    return response
+  end
+
   def say_intro
     sid = params[:CallSid]
     from = params[:From]
