ebe5d2ab0a810b73519c5f7e9590bb84bd257342
rememberlenny <rememberlenny@gmail.com>
2015-01-20 00:35:00 -0500

    Save the difference

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'admin' =>            'admin#dash'
 
   match 'get_id',          to: 'twilio_responses#get_id',          :via => [:post, :get]
-  match 'handle_response', to: 'twilio_responses#save',             :via => [:post, :get]
+  match 'handle_response', to: 'twilio_responses#save',            :via => [:post, :get]
   match 'check_recording', to: 'twilio_responses#check_recording', :via => [:post, :get]
   match 'check_response',  to: 'twilio_responses#check_response',  :via => [:post, :get]
   match 'after_recording', to: 'twilio_responses#after_recording', :via => [:post, :get]
