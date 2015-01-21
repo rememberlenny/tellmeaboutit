6a037e737df9b266532132e68c5c1dd78788f378
rememberlenny <rememberlenny@gmail.com>
2015-01-21 09:04:32 -0500

    Create up the controller into smaller parts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,35 @@
 class AdminController < ApplicationController
   def dash
-    @stories = Story.all
-    @recordings = Recording.all
-    @twiliocalls = TwilioCall.all
+    @content = full_accounts
+  end
+
+  def full_accounts accounts
     @accounts = Account.all
+
+    accounts.each do |account|
+      account.stories = []
+      full_stories account
+    end
+  end
+
+  def full_stories account
+    @stories = Story.all
+
+    @stories.each do |story|
+      if story.account_id == account.id
+        account.stories.push story
+        full_recording story
+      end
+    end
+  end
+
+  def full_recording story
+    @recordings = Recording.all
+
+    @recordings.each do |recording|
+      if recording.story_id == story.id
+        story.stories.push recording
+      end
+    end
   end
 end
