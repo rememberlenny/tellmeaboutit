eeb3916f7c9a2eb26154a2bf2f519b70b104e6db
rememberlenny <rememberlenny@gmail.com>
2015-01-23 00:57:54 -0500

    Add the migration for the recording sid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  account_sid = ENV['TWILIO_ACCOUNT_SID']
+  auth_token = ENV['TWILIO_ACCOUNT_TOKEN']
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122182614) do
+ActiveRecord::Schema.define(version: 20150123055742) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "uid"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150122182614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "story_id"
+    t.string   "sid"
   end
 
   create_table "stories", force: :cascade do |t|
