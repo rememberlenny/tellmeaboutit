40a735a3b21a9b7d75eb6ae936ae52433b5c7a6d
lamthuyvo <lamthuyvo@gmail.com>
2015-02-03 05:16:41 +0700

    added a bunch of styling to the pages and some content to the submit pages, also fixed some header stuff -- all while on a bumpy car ride in the Vietnamese countryside!

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,24 @@ $(document).ready(function(){
 	$('.breakup-container').css("width", screen_width);
 	$('.breakup-container').css("height", screen_height);
 	console.log(screen_height)
+
+	if (screen_width>600) {
+		//set the width and height of the poster image
+		$('.poster').css("width", screen_width);
+		$('.poster').css("height", screen_height);
+		
+		
+		//set the position of the opening text
+		$(".breakup-pullquote").css("top", screen_height/5.2);
+	} else {
+		//set the width and height of the poster image
+		$(".poster").css("height", "auto");
+
+		//set the position of the opening text
+		$(".breakup-pullquote").css("top", 10);
+
+	}
 	
-	$('.poster').css("width", screen_width);
-	$('.poster').css("height", screen_height);
-	
+
+		
 });
