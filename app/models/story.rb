89448ac328bd4ee575225adbe7d7260a9a9bcc99
rememberlenny <rememberlenny@gmail.com>
2015-02-07 10:18:14 -0500

    Create the next and prev button

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,12 @@
 class Story < ActiveRecord::Base
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   acts_as_taggable_on :breakup_type
+
+  def previous
+    Story.where(["id < ?", id]).last
+  end
+
+  def next
+    Story.where(["id > ?", id]).first
+  end
 end
