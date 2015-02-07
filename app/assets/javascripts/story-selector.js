330c68691b8aad0fd944600be9994e6d3ba369e6
rememberlenny <rememberlenny@gmail.com>
2015-02-07 09:44:14 -0500

    Create a selected story

---
+++ app/assets/javascripts/story-selector.js
@@ -0,0 +1,7 @@
+$('.specific-story').on('click', function(){
+  var name = 'Test';
+  var audio_url = 'http://api.twilio.com/2010-04-01/Accounts/ACb9f7fef2fed34f1092fd1ceac6b5212a/Recordings/RE285a5a99345a3878f59da8f9aec87ffd'
+  var template = "<div class='panel'><div class='row'><h5>" + name + "</h5></div><div class='row'><audio controls><source src=" + audio_url + " type='audio/mpeg'></audio></div><div class='row'><p>Share this story</p></div></div>"
+  $('.selected-story').html(template);
+});
+
