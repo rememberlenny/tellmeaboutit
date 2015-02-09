4e9f0ce9a5daeae40961104ad348f4bfdebfa654
rememberlenny <rememberlenny@gmail.com>
2015-02-09 18:09:23 -0500

    Remove the tags

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -70,7 +70,7 @@ class StoriesController < ApplicationController
     end
 
     def story_params
-      accessible = [ :name, :gender, :contact, :breakup_role, :notes, :person, :age, :location, :was_checked, :selected_recording_id, :tag_list => [], :type ] # extend with your own params
+      accessible = [ :name, :gender, :contact, :breakup_role, :notes, :person, :age, :location, :was_checked, :selected_recording_id, :type ] # extend with your own params
       params.require(:story).permit(accessible)
     end
 end
