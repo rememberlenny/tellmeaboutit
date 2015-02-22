e8f2e96b7ea46f7931f81c7d47b500ecef8b9c9c
rememberlenny <rememberlenny@gmail.com>
2015-02-22 11:42:04 -0500

    Create the relationships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222005128) do
+ActiveRecord::Schema.define(version: 20150222163730) do
+
+  create_table "stories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "gender"
+    t.integer  "age"
+    t.string   "location"
+    t.integer  "selected_recording_id"
+    t.string   "breakup_role"
+    t.text     "pullquote"
+    t.string   "breakup_type"
+    t.integer  "user_id"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  add_index "stories", ["user_id", "created_at"], name: "index_stories_on_user_id_and_created_at"
+  add_index "stories", ["user_id"], name: "index_stories_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
---
+++ spec/factories/stories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :story do
+    name "MyString"
+gender "MyString"
+age 1
+location "MyString"
+selected_recording_id 1
+breakup_role "MyString"
+pullquote "MyText"
+breakup_type "MyString"
+user nil
+  end
+
+end
