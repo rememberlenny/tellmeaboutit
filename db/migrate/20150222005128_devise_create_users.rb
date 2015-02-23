0518d9d87814af577d9459b4944f02d25f385c0d
rememberlenny <rememberlenny@gmail.com>
2015-02-22 19:40:19 -0500

    Update the users

--- db/migrate/20150222005128_devise_create_users.rb
+++ db/migrate/20150222005128_devise_create_users.rb
@@ -5,7 +5,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :phone,              null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :phone_verification_code, null: false, default: ""
+      t.boolean :phone_verified,          null: false, default: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
