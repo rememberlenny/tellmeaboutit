5e1fed9fed4a1495e02526db65b582bcf4cf919a
rememberlenny <rememberlenny@gmail.com>
2015-03-03 08:24:35 -0500

    Create the way for the recording to be displayed:

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,10 @@
 module StoriesHelper
+  def render_player story_id, recording_id
+    frame = '<iframe  class="recording"
+              src="http://www.wnyc.org/widgets/ondemand_player/#file=http://58c06a9f.ngrok.com/stories/' + story_id.to_s + '/recordings/' + recording_id.to_s + '.xml"
+              frameborder="0">
+    </iframe>'
+    raw frame
+  end
+
 end
