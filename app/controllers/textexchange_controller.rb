dde504d8317acbd191b73d7dcffbdd7cdc9365a6
rememberlenny <rememberlenny@gmail.com>
2015-03-17 19:22:52 -0400

    Make the functions even smaller

--- app/controllers/textexchange_controller.rb
+++ app/controllers/textexchange_controller.rb
@@ -45,11 +45,15 @@ class TextexchangeController < ApplicationController
     return action
   end
 
-  def send_action_sms action, thread_id
+  def get_phone_with_thread thread_id
     thread  = Textthread.find(thread_id)
     user    = User.find(thread.user_id)
+    return user.phone_number
+  end
 
-    send_message(user.phone_number, action)
+  def send_action_sms action, thread_id
+    phone = get_phone_with_thread(thread_id)
+    send_message(phone, action)
   end
 
   def create_story_with_thread thread_id
