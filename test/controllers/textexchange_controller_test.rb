402cd46db24a4e8e3d8f32f2923817cbb1cecd91
rememberlenny <rememberlenny@gmail.com>
2015-03-17 21:56:05 -0400

    Get into one working user"

--- test/controllers/textexchange_controller_test.rb
+++ test/controllers/textexchange_controller_test.rb
@@ -6,6 +6,13 @@ class TextexchangeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'find_user_from_phone helper' do
+    user = TextexchangeHelper.find_user_from_phone nil
+    if user == nil
+      assert_response :success
+    end
+  end
+
   test 'check textexchange#text_delegate' do
     get :text_delegate
     assert_response :success
