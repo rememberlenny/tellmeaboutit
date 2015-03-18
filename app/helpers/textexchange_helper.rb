83aa887516fd2f6e6aa1629ec4964454f0a9c733
rememberlenny <rememberlenny@gmail.com>
2015-03-18 17:18:43 -0400

    Refine the tests

--- app/helpers/textexchange_helper.rb
+++ app/helpers/textexchange_helper.rb
@@ -2,7 +2,7 @@ module TextexchangeHelper
 
   def self.find_user_from_phone phone_number
     users = User.where(:phone_number => phone_number)
-    if users.count > 0
+    if users.count > 1
       user = user.first
     else
       user = nil
--- test/controllers/textexchange_controller_test.rb
+++ test/controllers/textexchange_controller_test.rb
@@ -6,15 +6,30 @@ class TextexchangeControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test 'find_user_from_phone helper' do
+  test 'find_user_from_phone helper with nil' do
     user = TextexchangeHelper.find_user_from_phone nil
     if user == nil
       assert_response :success
+    else
+      assert_response :failure
+    end
+  end
+
+  test 'find_user_from_phone helper with user' do
+    User.delete_all
+    u = User.new(phone_number: '+19493228496', :password => 'password123')
+    u.save
+    user = TextexchangeHelper.find_user_from_phone('+19493228496')
+    if user != nil
+      assert_response :success
+      assert_response :success
+    else
+      assert_response :failure
     end
   end
 
   test 'check textexchange#text_delegate' do
-    get :text_delegate
+
     assert_response :success
   end
 
