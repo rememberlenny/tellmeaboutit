2866666ee093d89d48832a698b0110c8df51c562
rememberlenny <rememberlenny@gmail.com>
2015-03-19 09:24:41 -0400

    Add TTT

--- spec/controllers/textexchange_controller_spec.rb
+++ spec/controllers/textexchange_controller_spec.rb
@@ -16,4 +16,32 @@ RSpec.describe TextexchangeController, type: :controller do
     end
   end
 
+
+  describe 'find_user_from_phone helper with nil' do
+    user = TextexchangeHelper.find_user_from_phone nil
+    if user == nil
+      assert_response :success
+    else
+      assert_response :failure
+    end
+  end
+
+  describe 'find_user_from_phone helper with user' do
+    User.delete_all
+    u = User.new(phone_number: '+19493228496', :password => 'password123')
+    u.save
+    user = TextexchangeHelper.find_user_from_phone('+19493228496')
+    if user != nil
+      assert_response :success
+    else
+      assert_response :failure
+    end
+  end
+
+  describe 'check textexchange#text_delegate' do
+
+    assert_response :success
+  end
+
+
 end
