1d8849bb983d0e6feb5802c2136d4abd806d27bf
rememberlenny <rememberlenny@gmail.com>
2015-03-20 18:55:01 -0400

    Get the existing user and create new user behavior

--- app/helpers/textexchange_helper.rb
+++ app/helpers/textexchange_helper.rb
@@ -3,7 +3,7 @@ module TextexchangeHelper
   def self.find_user_from_phone phone_number
     phone_number = phone_number.gsub('+', '')
     users = User.where(:phone_number => phone_number)
-    if users.count > 1
+    if users.count > 0
       user = users.first
       uid = user.id
     else
@@ -14,7 +14,7 @@ module TextexchangeHelper
 
   def self.create_user_with_number phone_number
     puts 'Create user with number'
-    user = User.new(:phone_number => phone_number, :password => '123')
+    user = User.new(:phone_number => phone_number, :password => '123123123')
     user.save
     return user.id
   end
