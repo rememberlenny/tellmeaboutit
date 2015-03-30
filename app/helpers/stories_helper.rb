a2405f3a532a991fd00d1bf0550586f3929fa726
rememberlenny <rememberlenny@gmail.com>
2015-03-30 09:32:53 -0400

    Save the tell me about it recording view

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -2,7 +2,7 @@ module StoriesHelper
   def render_player recording_url
     frame = '<iframe  class="recording"
               src="http://www.wnyc.org/widgets/ondemand_player/#file=' + recording_url +
-              'frameborder="0">
+              ' frameborder="0">
     </iframe>'
     raw frame
   end
